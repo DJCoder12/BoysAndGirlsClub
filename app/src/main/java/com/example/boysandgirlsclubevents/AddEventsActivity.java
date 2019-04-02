be7f421aaba43826d9a6fa405d53e2eb04e187bd
Joseph Nguyen <jnguyen2@fandm.edu>
2019-04-02 16:16:41 -0400

    Fix layout constraints and begin work on TimePicker

--- app/src/main/java/com/example/boysandgirlsclubevents/AddEventsActivity.java
+++ app/src/main/java/com/example/boysandgirlsclubevents/AddEventsActivity.java
@@ -1,7 +1,9 @@
 package com.example.boysandgirlsclubevents;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddEventsActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class AddEventsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_events);
+    }
+
+    public void onClickTest(View v) {
+         DialogFragment df = new TimePickerFragment();
+         df.show(getSupportFragmentManager(), "Time Picker");
     }
 }
