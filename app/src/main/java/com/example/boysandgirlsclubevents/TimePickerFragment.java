0bc98236687bd3c9c1a4485a9fafeb4cebd6502c
Joseph Nguyen <jnguyen2@fandm.edu>
2019-04-02 16:17:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/boysandgirlsclubevents/TimePickerFragment.java
@@ -0,0 +1,30 @@
+package com.example.boysandgirlsclubevents;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date and time values in the picker.
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // TODO: do something with the time set by the user.
+    }
+}
