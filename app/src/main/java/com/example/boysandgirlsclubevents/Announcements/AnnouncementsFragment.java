225307dae2c9824b43a0db4e2d533263afed9c8b
DJCoder12 <donj1220@yahoo.com>
2019-04-09 12:39:01 -0400

    announcments with test statements. Needs to be adjusted for fragments

--- app/src/main/java/com/example/boysandgirlsclubevents/Announcements/AnnouncementsFragment.java
+++ app/src/main/java/com/example/boysandgirlsclubevents/Announcements/AnnouncementsFragment.java
@@ -3,26 +3,77 @@ package com.example.boysandgirlsclubevents.Announcements;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.boysandgirlsclubevents.R;
+import com.example.boysandgirlsclubevents.RecyclerViewAdapter;
+
+import java.util.ArrayList;
 
 public class AnnouncementsFragment extends Fragment
 {
+
     public static String TAG = "AnnouncementsFragment";
 
+        private ArrayList<String> statements = new ArrayList<String>();
+        private ArrayList<String> imageUrls = new ArrayList<String>();
+
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.fragment_announcements);
+        Log.d(TAG, "onCreate: started.");
+
+        initImageBitmaps();
+        Log.d(TAG, "initImage passed.");
+        return inflater.inflate(R.layout.fragment_announcements, container, false);
+
+
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
-        return inflater.inflate(R.layout.fragment_announcements, container, false);
+    private void initImageBitmaps() {
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps");
+
+        statements.add("This is a sample announcement");
+        imageUrls.add("https://positivecoach.org/media/805204/bcga.png");
+
+        statements.add("This is a sample announcement");
+        imageUrls.add("https://positivecoach.org/media/805204/bcga.png");
+
+        statements.add("This is a sample announcement");
+        imageUrls.add("https://positivecoach.org/media/805204/bcga.png");
+
+        statements.add("This is a sample announcement");
+        imageUrls.add("https://positivecoach.org/media/805204/bcga.png");
+
+        statements.add("This is a sample announcement");
+        imageUrls.add("https://positivecoach.org/media/805204/bcga.png");
+
+        statements.add("This is a sample announcement");
+        imageUrls.add("https://positivecoach.org/media/805204/bcga.png");
+
+        statements.add("This is a sample announcement");
+        imageUrls.add("https://positivecoach.org/media/805204/bcga.png");
+
+        statements.add("This is a sample announcement");
+        imageUrls.add("https://positivecoach.org/media/805204/bcga.png");
+
+
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        RecyclerView recyclerView = getView().findViewById(R.id.my_recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(statements, imageUrls, getActivity());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 }
---
+++ app/src/main/java/com/example/boysandgirlsclubevents/RecyclerViewAdapter.java
@@ -0,0 +1,81 @@
+package com.example.boysandgirlsclubevents;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+    private static final String TAG = "RecyclerView";
+    private  ArrayList<String> appImages = new ArrayList<String>();
+    private ArrayList<String> appAnnouncements = new ArrayList<String>();
+    private Context appContext;
+
+    public RecyclerViewAdapter(ArrayList<String> announcements, ArrayList<String> images, Context context){
+        appAnnouncements = announcements;
+        appContext = context;
+        appImages = images;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, final int i) {
+        Log.d(TAG,"onBindViewHolder: called.");
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"onClick: clicked on some text");
+                Glide.with(appContext)
+                        .asBitmap()
+                        .load(appImages.get(i))
+                        .into(viewHolder.image);
+                viewHolder.text.setText(appAnnouncements.get(i));
+
+                viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d(TAG,"onClick:clicked on: "+ appAnnouncements.get(i));
+                        Toast.makeText(appContext, "this is happening", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return appAnnouncements.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        CircleImageView image;
+        TextView text;
+        RelativeLayout parentLayout;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            text = itemView.findViewById(R.id.announcement);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+
+        }
+    }
+}
