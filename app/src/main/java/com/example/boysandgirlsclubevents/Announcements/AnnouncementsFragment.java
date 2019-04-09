316f1ad1fa597414059dd2940148c02a456f714d
DJCoder12 <donj1220@yahoo.com>
2019-04-09 12:57:15 -0400

    recycler view has a display now still needs to be stylized

--- app/src/main/java/com/example/boysandgirlsclubevents/Announcements/AnnouncementsFragment.java
+++ app/src/main/java/com/example/boysandgirlsclubevents/Announcements/AnnouncementsFragment.java
@@ -27,19 +27,22 @@ public class AnnouncementsFragment extends Fragment
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
-        super.onCreate(savedInstanceState);
-        //setContentView(R.layout.fragment_announcements);
-        Log.d(TAG, "onCreate: started.");
-
-        initImageBitmaps();
-        Log.d(TAG, "initImage passed.");
         return inflater.inflate(R.layout.fragment_announcements, container, false);
 
 
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //setContentView(R.layout.fragment_announcements);
+        Log.d(TAG, "SSS onCreate: started.");
+        initImageBitmaps();
+        Log.d(TAG, "initImage passed.");
+    }
+
     private void initImageBitmaps() {
-        Log.d(TAG, "initImageBitmaps: preparing bitmaps");
+        Log.d(TAG, "SSS initImageBitmaps: preparing bitmaps");
 
         statements.add("This is a sample announcement");
         imageUrls.add("https://positivecoach.org/media/805204/bcga.png");
@@ -65,15 +68,17 @@ public class AnnouncementsFragment extends Fragment
         statements.add("This is a sample announcement");
         imageUrls.add("https://positivecoach.org/media/805204/bcga.png");
 
-
+        Log.d(TAG, " SSS initImages added.");
 
         initRecyclerView();
     }
 
     private void initRecyclerView() {
         RecyclerView recyclerView = getView().findViewById(R.id.my_recycler_view);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(statements, imageUrls, getActivity());
+        Log.d(TAG, "SSS VIEW MADE .");
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(statements, imageUrls, getContext());
         recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        Log.d(TAG, "SSS adapter set.");
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }
