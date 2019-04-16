58e0c5827156764d8686cda5a1400587dbeaf248
Jordan Cupid <jcupid@fandm.edu>
2019-04-16 03:06:27 -0400

    Overhauled display for member of the month with CardView; still need to hook up to database

--- app/src/main/java/com/example/boysandgirlsclubevents/MemberMonthFragment.java
+++ app/src/main/java/com/example/boysandgirlsclubevents/MemberMonthFragment.java
@@ -10,9 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class MemberMonthFragment extends Fragment
 {
     public static String TAG = "MemberMonthFragment";
+    ArrayList<MemberMonth> members;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState)
@@ -32,18 +35,9 @@ public class MemberMonthFragment extends Fragment
         recyclerView.setLayoutManager(layoutManager);
 
 
-        String[] title = new String[]{
-                "Ashlen is a new member to the club and has made a great impression since she started! Each day she participates in Power Hour where she completes her homework and tutors others. Her favorite activity to help with is Supper Club! Ashlen has a cheerful personality that draws in other members as she models responsible and respectful behavior!",
-                "Carlos is a long-time member that we love having around our Club! He participated in Flag Football and Passport to Manhood during the Fall and is looking forward to being involved with Jack Walker’s Basketball League. Carlos does a great job of motivating his fellow members and inspiring them to also be involved in the Club. He is a great mentor to younger members by modeling respectful and responsible behavior every day."
-        };
-
-        int [] picturePath = new int[]{
-                R.drawable.ashlencurry,
-                R.drawable.carloscuevas
-        };
-
-        ListAdapter listAdapter = new ListAdapter(title, picturePath);
-        recyclerView.setAdapter(listAdapter);
+        members = MemberMonth.createMembersList(20);
+        MemberAdapter memberAdapter = new MemberAdapter(members);
+        recyclerView.setAdapter(memberAdapter);
 
 
         return rootView;
